ad4d31da1ad4807fb3696d97721fcd0d8d82d841
dzc <dzc_ze@foxmail.com>
2016-01-29 11:07:49 +0800

    Initial Commit

---
+++ ganhuoSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ganhuoSwift
+//
+//  Created by zajo on 16/1/29.
+//  Copyright © 2016年 dzc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ganhuoSwiftTests/ganhuoSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  ganhuoSwiftTests.swift
+//  ganhuoSwiftTests
+//
+//  Created by zajo on 16/1/29.
+//  Copyright © 2016年 dzc. All rights reserved.
+//
+
+import XCTest
+@testable import ganhuoSwift
+
+class ganhuoSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
