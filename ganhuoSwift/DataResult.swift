e6519e14ef700d5a30f166643ddf20e484dea193
dzc <dzc_ze@foxmail.com>
2016-02-01 18:31:32 +0800

    update

---
+++ ganhuoSwift/DataResult.swift
@@ -0,0 +1,17 @@
+//
+//  ESRootClass.swift
+//  ganhuoSwift
+//
+//  Created by zajo on 16/02/01
+//  Copyright (c) dzc. All rights reserved.
+//
+
+import UIKit
+
+class DataResult: NSObject {
+
+    var error: Bool = false
+
+    var results: [ResultItem]?
+
+}
---
+++ ganhuoSwift/ResultItem.swift
@@ -0,0 +1,31 @@
+//
+//  Results.swift
+//  ganhuoSwift
+//
+//  Created by zajo on 16/02/01
+//  Copyright (c) dzc. All rights reserved.
+//
+
+import UIKit
+
+class ResultItem: NSObject {
+
+    var updatedAt: String?
+
+    var who: String?
+
+    var publishedAt: String?
+
+    var objectId: String?
+
+    var used: Bool = false
+
+    var type: String?
+
+    var createdAt: String?
+
+    var desc: String?
+
+    var url: String?
+
+}
